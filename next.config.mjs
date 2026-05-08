cb4e3d31b144fbfe7fd11040f7a4bd299bd1a325
Hedinyer Perucho <alucardperucho@gmail.com>
2026-05-08 11:00:32 -0500

    chore: Remove ESLint ignore setting from Next.js configuration and specify Node.js engine version in package.json for better compatibility.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
